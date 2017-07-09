16d5f98286843605ebdba7b7e6ad1327f06597fb
Anthony Petrides <tony@tonysdopelemons.home>
2017-07-09 11:10:47 +0100

    section 2 up to lecture 25

--- src/index.js
+++ src/index.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyBut7cRnFOS3cCgvxQ8h3y-svndtYpuUWU';
 
-YTSearch ({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
 
 //Create a new component. This component should produce
 //some HTML
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch ({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      console.log(videos);
+      // short hand for - this.setState ({videos: videos});
+    });
+  }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
         </div>
     );
   }
